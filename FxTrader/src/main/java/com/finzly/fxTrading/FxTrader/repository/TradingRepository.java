e3c1f8a49deec2448dbd6d0d7ecd28c7e00a1b04
Anandh2000 <110973366+Anandh2000@users.noreply.github.com>
2022-10-10 12:27:14 +0530

    Delete FxTrader directory

--- FxTrader/src/main/java/com/finzly/fxTrading/FxTrader/repository/TradingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.finzly.fxTrading.FxTrader.repository;
-
-import com.finzly.fxTrading.FxTrader.entity.FxTradingData;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TradingRepository extends JpaRepository<FxTradingData, Integer>{
-
-}
